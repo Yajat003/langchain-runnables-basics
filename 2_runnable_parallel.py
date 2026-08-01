d527c7aba1ac8b5f8d03c475b0831f55996188e9
YDP <yajat1003@gmail.com>
2026-08-01 12:53:37 +0530

    Basics: Runnable sequence & parallel in LangChain

---
+++ 2_runnable_parallel.py
@@ -0,0 +1,25 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableParallel, RunnableSequence
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model= "gemini-2.5-flash")
+
+prompt_1 = PromptTemplate(template= "Generate a tweet about {topic}", 
+                          input_variables= ['topic'])
+
+prompt_2 = PromptTemplate(template= "Generate a linkedin post about {topic}",
+                          input_variables= ['topic'])
+
+parser = StrOutputParser()
+
+parallel_chain = RunnableParallel({'tweet': RunnableSequence(prompt_1, model, parser),
+                                  'linkedin': RunnableSequence(prompt_2, model, parser)})
+
+result = parallel_chain.invoke({'topic': 'AI'})
+
+print("Generated Tweet: ", result['tweet'])
+print("Generated LinkedIn post: ", result['linkedin'])
